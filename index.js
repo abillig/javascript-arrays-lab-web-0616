4748571d5d1575c3735595a54c8f4e4d7bba0931
abillig <avrambillig@gmail.com>
2016-07-20 00:27:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const app = "I don't do much."
+
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop(-1);
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift(0);
+}
+
+function appendKitten(name){
+  kittens = [...kittens, name];
+  return kittens;
+}
+
+function prependKitten(name){
+  kittens = [name, ...kittens];
+  return kittens;
+}
+
+function removeLastKitten(){
+  kittens = kittens.slice(0, kittens.length - 1);
+  return kittens;
+}
+
+function removeFirstKitten(){
+  kittens = kittens.slice(1);
+  return kittens;
+}
